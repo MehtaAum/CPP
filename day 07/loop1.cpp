7bf5e4e71d1168e7c0d1296abd1de61d8ba7af2e
Mehta Aum <maum5302@gmail.com>
2024-12-16 15:46:25 +0530

    Add files via upload

---
+++ day 07/loop1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//print 1 to 100 using while loop.
+int main (){
+    
+    int s = 1, e = 100;
+
+    while (s <= e)
+    {
+        cout<<s<<endl;
+        s++;//increment
+    }
+    
+
+}
