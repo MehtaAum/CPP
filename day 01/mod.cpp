8ec67515738d6615688c66747c288b764943e2a5
Mehta Aum <maum5302@gmail.com>
2024-12-16 15:42:56 +0530

    Add files via upload

---
+++ day 01/mod.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 60;
+    int y = 50;
+
+    cout<< 60%50;
+
+}
---
+++ day 01/sum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x = 60, y = 40;
+    cout<<"xy : ";
+    cout<< x + y ;
+}
