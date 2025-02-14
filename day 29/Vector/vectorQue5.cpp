e6ba262c4a1d7f1013de2b78f2e2d96038f01466
Mehta Aum <maum5302@gmail.com>
2025-02-14 20:20:42 +0530

    Vector Practice

---
+++ day 29/Vector/vectorQue5.cpp
@@ -0,0 +1,10 @@
+//find the difference between the sum of elements at even indices to the sum of elements at odd indices
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //this program is currently on building stage.
+
+}
